132a6d1d8490a7a18a7b1a7ca495f3f6ec81c258
dungnd1608 <96166965+dungnd1608@users.noreply.github.com>
2023-11-23 11:25:44 +0700

    Add files via upload

---
+++ a.py
@@ -0,0 +1,29 @@
+import pypyodbc
+from flask import Flask, request, render_template, redirect, url_for, session
+app = Flask(__name__)
+server = "117.103.207.96,9979"
+database = "dung"
+username = "dungnd"
+password = "Qtdv@@20@!"
+connection = pypyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = connection.cursor()
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+if __name__ == '__main__':
+    app.run('0.0.0.0')
